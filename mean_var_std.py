3cbe0d4b7f549df8036b23dc564a6c24e540634e
Replit user <>
2023-04-26 09:20:02 -0500

    first commit

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,26 @@
 import numpy as np
 
 def calculate(list):
+  if len(list) < 9:
+    raise ValueError("List must contain nine numbers.")
+  arr = np.asarray(list)
+  arr = arr.reshape((3,3))
+  mean = [(arr.mean(axis = 0)).tolist(), (arr.mean(axis=1)).tolist(), (np.mean(arr.flatten())).tolist()]
+  variance = [(arr.var(axis = 0)).tolist(),(arr.var(axis=1)).tolist(), (np.var(arr.flatten())).tolist()]
+  std = [(arr.std(axis = 0)).tolist(), (arr.std(axis=1)).tolist(), (np.std(arr.flatten())).tolist()]
+  max = [(arr.max(axis = 0)).tolist(), (arr.max(axis=1)).tolist(), (np.max(arr.flatten())).tolist()]
+  min = [(arr.min(axis = 0)).tolist(), (arr.min(axis=1)).tolist(), (np.min(arr.flatten())).tolist()]
+  sum = [(arr.sum(axis = 0)).tolist(), (arr.sum(axis=1)).tolist(), (np.sum(arr.flatten())).tolist()]
+
+  calculations = {
+    "mean":mean,
+    "variance":variance,
+    "standard deviation":std,
+    "max":max,
+    "min":min,
+    "sum":sum}
+  
 
 
 
-
-    return calculations
+  return calculations
